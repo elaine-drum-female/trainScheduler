4f635b53445b5aee2c81600f4465fba0b2c59e06
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-01-21 22:00:24 -0600

    established the firebase config

--- assets/js/train-app.js
+++ assets/js/train-app.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkEzDr8kSQyMzV6_kQlt-T54QatAFntbY",
+    authDomain: "trainscheduler-ae0b5.firebaseapp.com",
+    databaseURL: "https://trainscheduler-ae0b5.firebaseio.com",
+    projectId: "trainscheduler-ae0b5",
+    storageBucket: "trainscheduler-ae0b5.appspot.com",
+    messagingSenderId: "911191489095"
+  };
+  firebase.initializeApp(config);
 });
