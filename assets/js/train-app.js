b80255cb7b6bd456e964d86c5271b64c1ef1ac47
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-01-21 22:27:15 -0600

    added variables

--- assets/js/train-app.js
+++ assets/js/train-app.js
@@ -9,4 +9,84 @@ $(document).ready(function () {
     messagingSenderId: "911191489095"
   };
   firebase.initializeApp(config);
+    
+  // add variables
+
+var trainName = "";
+var destination = "";
+var firstTime = "05:00";
+var firstTimeConverted = moment(firstTime, "HH:mm").substract(1, "years");
+var currentTime = moment();
+var tFrequency: 10;
+var diffTime = moment().diff(moment(firstTimeConverted, "minutes");
+var tRemainder = diffTime % tFrequency;
+var tMinutestilTrain = tFrequency - tRemainder;
+var nextArrival= moment().add(tMinutestilTrain, "minutes");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
